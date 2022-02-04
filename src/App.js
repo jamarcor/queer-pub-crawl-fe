01dff5c1cfb6f3f7fad41f4c3045778f5f454d79
James Correa <james.correa9090@gmail.com>
2022-02-04 08:47:00 -1000

    login and auth stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./style/App.css";
+import Header from "./components/Header";
+import SignInForm from "./components/SignInForm";
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const [user, setUser] = useState({});
+  const [form, setForm] = useState("");
+
+
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetch(`http://localhost:3000/auto_login`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          setUser(data);
+          console.log(data)
+        });
+    }
+  }, []);
+
+  const handleLogin = (user) => {
+    setUser(user);
+  };
+
+  const handleFormSwitch = (input) => {
+    setForm(input);
+  };
+
+
+  console.log(user);
+
+  const renderForm = () => {
+    switch (form) {
+      case "login":
+        return <LoginForm handleLogin={handleLogin} />;
+        break;
+      default:
+        return <SignInForm handleLogin={handleLogin} />;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header handleFormSwitch={handleFormSwitch} />
+      {renderForm()}
+      <button className="ui button">
+        Home Page
+      </button>
     </div>
   );
 }
